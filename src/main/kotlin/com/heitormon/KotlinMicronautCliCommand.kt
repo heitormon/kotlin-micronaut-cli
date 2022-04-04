4ab8a55ea697d6ca221120558d2a88f9ded2f087
Heitor Nobuo <heitor.nozu@ebanx.com>
2022-04-04 10:42:59 -0300

    frst commit

---
+++ src/main/kotlin/com/heitormon/KotlinMicronautCliCommand.kt
@@ -0,0 +1,30 @@
+package com.heitormon
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "kotlin-micronaut-cli", description = ["Simple Math Operations"],
+        mixinStandardHelpOptions = true)
+class KotlinMicronautCliCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(KotlinMicronautCliCommand::class.java, *args)
+        }
+    }
+}
